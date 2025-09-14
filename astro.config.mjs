092686a753607e2ce55143618fcdab0985b6943e
Jason Kyle De Lara <jason8oh8@gmail.com>
2025-09-13 19:33:09 -0700

    fixed the nginx config since it was messing up my site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   	integrations: [tailwind()],
-	server: { host: true }, // WTF why is this needed?
+	server: { host: true },
 });
